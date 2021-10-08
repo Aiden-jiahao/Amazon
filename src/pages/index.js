2c29309e7a53312d61404097e60272f869ef0ea0
Aiden <aidenjiahao@gmail.com>
2021-10-08 14:10:41 -0400

    success up until 1:45

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,38 @@
 import Head from "next/head";
 import Header from "../components/Header";
-export default function Home() {
+import Banner from "../components/Banner";
+import ProductFeed from "../components/ProductFeed";
+
+export default function Home({ products }) {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
+
       {/* Header*/}
       <Header />
+
       {/* Bannner*/}
+      <main className="max-w-screen-2xl mx-auto">
+        {/* Bannner*/}
+        <Banner />
+        {/*Product Feed */}
+        <ProductFeed products={products} />
+      </main>
     </div>
   );
+}
+
+// next.js things, render the page on server not on user side.
+export async function getServerSideProps(context) {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      products,
+    },
+  };
 }
