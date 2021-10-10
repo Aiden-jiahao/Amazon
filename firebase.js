e7a632937fd2116445fe18783b41986c30bc74f6
Aiden <aidenjiahao@gmail.com>
2021-10-09 22:17:23 -0400

    success with day 3

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDDu5jZuvnN1rRGhZ-NlW6DV7XBJeuGWYY",
+  authDomain: "fir-1e790.firebaseapp.com",
+  projectId: "fir-1e790",
+  storageBucket: "fir-1e790.appspot.com",
+  messagingSenderId: "173514200183",
+  appId: "1:173514200183:web:3dc6fa7f3b7e1c1210a272",
+};
--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import basketReducer from "../slices/basketSlice";
 
+// this is the global store, ex onion slice
 export const store = configureStore({
   reducer: {
     basket: basketReducer,
