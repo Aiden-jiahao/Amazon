698b515b65e97acf6a69d4edee6902ad3bb55824
Aiden <aidenjiahao@gmail.com>
2021-10-11 11:29:19 -0400

    no message

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -1,6 +1,17 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 export default async (req, res) => {
   const { items, email } = req.body;
   console.log(items);
   console.log(email);
+  const transformedItems = items.map((item) => ({
+    price_data: {
+      currency: "CAD",
+      unit_amount: item.price * 100,
+      product_data: {
+        name: item.title,
+        images: [item.image],
+      },
+    },
+  }));
 };
